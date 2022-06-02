b8d90bd05edbc5919432f7954408cd81b72f866f
Grigiryitstud <initstudentitin@gmail.com>
2022-06-02 17:34:45 +0300

    refactor: main page is done (Thu, Jun 02, 2022 05:34:44 PM)

--- gov-app/src/States/States.js
+++ gov-app/src/States/States.js
@@ -4,18 +4,19 @@ import s from './States.module.css';
 const States = () => {
   return (
     <div className={s.intro}>
-    <div className={s.container}>
-       <div className={s.intro__inner}>
-            <h1 className={s.intro__titles}>
-            Peoples must respect government, government must afraid peoples
-            </h1>
-            <h2 className={s.intro__subtitles}>
-            демократия и образование спасут мир
-            </h2>
-            <a className={s.btn} href="">Узнай больше</a>
-        </div>
+      <div className={s.intro__inner}>
+        <h2 className={s.intro__subtitles}>
+          Географический центр Киевского княжества, культурный центр восточных славян(средневековье), старейший город РБ
+        </h2>
+        <h2 className={s.intro__subtitles}>
+          Новоградок, центр объединения земель в Великое княжества Литовское, Русское и Жемайтское (спасло русь от орды)
+        </h2>
+        <h2 className={s.intro__subtitles}>
+          Исторический центр полесья, многие считают местом возникновения славян(печки каменки и т.п. находили здесь первые)
+        </h2>
+        <a className={s.btn} href="">Узнай больше</a>
+      </div>
     </div>
-</div>
   );
 }
 
