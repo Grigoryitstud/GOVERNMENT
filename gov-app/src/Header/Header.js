5ba61745e0d73d5774202de4042c9b2bcb3032ee
Grigiryitstud <initstudentitin@gmail.com>
2022-06-17 23:11:40 +0300

    feat: main page (Fri, Jun 17, 2022 11:11:40 PM)

--- gov-app/src/Header/Header.js
+++ gov-app/src/Header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
           </div>
 
           <nav className={s.nav}>
-            <NavLink to='' className = { navData => navData.isActive ? s.active : s.item }>ГЛАВНАЯ</NavLink>
+            <NavLink to='/GOVERNMENT' className = { navData => navData.isActive ? s.active : s.item }>ГЛАВНАЯ</NavLink>
             <NavLink to='/DemPage' className = { navData => navData.isActive ? s.active : s.item }>ДЕМОКРАТИЯ</NavLink>
             <NavLink to='/MistoPage' className = { navData => navData.isActive ? s.active : s.item }>МЯСТЭЧКИ</NavLink>
             <NavLink to='/GovePage' className = { navData => navData.isActive ? s.active : s.item }>Правительство</NavLink>
